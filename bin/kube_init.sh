648e5eef9626d11ab6528bb1ba2ba65a65d42a16
childsb <bchilds@redhat.com>
2017-11-09 21:52:20 -0600

    add some stuf

---
+++ bin/kube_init.sh
@@ -0,0 +1,9 @@
+#@/bin/sh
+cd /opt/go/src/k8s.io/kubernetes/
+
+  export KUBERNETES_PROVIDER=local
+
+  cluster/kubectl.sh config set-cluster local --server=https://localhost:6443 --certificate-authority=/var/run/kubernetes/server-ca.crt
+  cluster/kubectl.sh config set-credentials myself --client-key=/var/run/kubernetes/client-admin.key --client-certificate=/var/run/kubernetes/client-admin.crt
+  cluster/kubectl.sh config set-context local --cluster=local --user=myself
+  cluster/kubectl.sh config use-context local
